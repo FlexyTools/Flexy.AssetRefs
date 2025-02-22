2858d0a0db755c9f8a2d00dea4df238ebe4c4683
JesOb <JesObertyukh@gmail.com>
2025-02-22 23:18:57 +0100

    Clean up

--- Runtime/AssetRef.cs
+++ Runtime/AssetRef.cs
@@ -1,13 +1,16 @@
 global using System;
 global using System.Collections.Generic;
-global using Cysharp.Threading.Tasks;
+
 global using UnityEngine;
 global using UnityEngine.SceneManagement;
 global using UnityEngine.Pool;
-global using Object = UnityEngine.Object;
 
-using Flexy.AssetRefs.AssetLoaders;
-using Flexy.Serialisation;
+global using Cysharp.Threading.Tasks;
+
+global using Flexy.AssetRefs.AssetLoaders;
+global using Flexy.Serialisation;
+
+global using Object = UnityEngine.Object;
 
 namespace Flexy.AssetRefs;
 
