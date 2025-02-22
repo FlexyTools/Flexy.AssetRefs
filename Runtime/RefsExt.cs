af6be280da5db1677875498a0c50262d1f4435f0
JesOb <JesObertyukh@gmail.com>
2025-02-22 23:23:02 +0100

    Clean up

--- Runtime/RefsExt.cs
+++ Runtime/RefsExt.cs
@@ -2,14 +2,11 @@ namespace Flexy.AssetRefs;
 
 public static class RefsExt
 {
-   	public static			T?					LoadAssetSync<T>		( this AssetRef<T> @ref ) where T : Object														=> AssetRef.AssetsLoader.LoadAssetSync<T>( @ref );
-    public static			UniTask<T?>			LoadAssetAsync<T>		( this AssetRef<T> @ref ) where T : Object														=> AssetRef.AssetsLoader.LoadAssetAsync<T>( @ref );
+   	public static	T?				LoadAssetSync<T>	( this AssetRef<T> @ref ) where T : Object							=> AssetRef.AssetsLoader.LoadAssetSync<T>( @ref );
+    public static	UniTask<T?>		LoadAssetAsync<T>	( this AssetRef<T> @ref ) where T : Object							=> AssetRef.AssetsLoader.LoadAssetAsync<T>( @ref );
 
-    public static			String?				GetSceneName			( this SceneRef @ref )																			=> AssetRef.AssetsLoader.GetSceneName( @ref );
-	public static			SceneTask			LoadSceneAsync			( this SceneRef @ref, GameObject context, LoadSceneMode loadMode = LoadSceneMode.Additive )		=> AssetRef.AssetsLoader.LoadSceneAsync( @ref, new (loadMode), context );
-	public static			SceneTask			LoadSceneAsync			( this SceneRef @ref, GameObject context, LoadSceneParameters p )								=> AssetRef.AssetsLoader.LoadSceneAsync( @ref, new(p.loadSceneMode, p.localPhysicsMode), context );
-	public static			SceneTask			LoadSceneAsync			( this SceneRef @ref, GameObject context, SceneTask.Parameters p )								=> AssetRef.AssetsLoader.LoadSceneAsync( @ref, p, context );
-	
-	public static			T?					LoadAssetTypedSync<T>	( this AssetRef @ref ) where T : Object	=> new AssetRef<T>( @ref.Uid, @ref.SubId ).LoadAssetSync( );
-	public static			UniTask<T?>			LoadAssetTyped<T>		( this AssetRef @ref ) where T : Object	=> new AssetRef<T>( @ref.Uid, @ref.SubId ).LoadAssetAsync( );
+    public static	String?			GetSceneName		( this SceneRef @ref )												=> AssetRef.AssetsLoader.GetSceneName( @ref );
+	public static	SceneTask		LoadSceneAsync		( this SceneRef @ref, GameObject context, LoadSceneParameters p )	=> AssetRef.AssetsLoader.LoadSceneAsync( @ref, new(p.loadSceneMode, p.localPhysicsMode), context );
+	public static	SceneTask		LoadSceneAsync		( this SceneRef @ref, GameObject context, SceneTask.Parameters p )	=> AssetRef.AssetsLoader.LoadSceneAsync( @ref, p, context );
+	public static	SceneTask		LoadSceneAsync		( this SceneRef @ref, GameObject context, LoadSceneMode loadMode = LoadSceneMode.Additive )		=> AssetRef.AssetsLoader.LoadSceneAsync( @ref, new (loadMode), context );
 }
