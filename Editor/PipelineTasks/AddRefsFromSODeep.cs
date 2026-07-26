30e3310a12669adaca78029e10978de669320714
JesOb <JesObertyukh@gmail.com>
2026-07-26 23:15:54 +0200

    Added ready to use Task that will collect refs from Scriptable object with deep traversal on all fields

---
+++ Editor/PipelineTasks/AddRefsFromSODeep.cs
@@ -0,0 +1,17 @@
+namespace Flexy.AssetRefs.Editor.PipelineTasks;
+
+public class AddRefsFromSODeep : IPipelineTask
+{
+	[SerializeField] ScriptableObject _source = null!;
+
+	public void Run( Pipeline ppln, Context ctx )
+	{
+		if (!_source)
+		{
+			Debug.LogError($"[{nameof(AddRefsFromSODeep)}] Source ScriptableObject is not assigned.", ppln);
+			return;
+		}
+
+		ctx.Get<RefsList>().AddRange(RefsCollector.CollectRefsDeep(_source));
+	}
+}
